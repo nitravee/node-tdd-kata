3b22460298eac5ab15e7eae2bf547b7024940d1e
Nitravee Wongvitavas <nitravee@Nitravees-MacBook-Pro.local>
2018-08-21 15:49:32 +0700

    First bullet

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // App code here
+
+function stringCal(str){
+    if(str.includes(',')){
+        return str.split(',').map(Number).reduce((x,y) => x+y,0)
+    }
+    if(str === ''){
+        return 0
+    }
+    if(!Number.isNaN(str)){
+        return parseInt(str)
+    }
+}
+
+
+module.exports.strCal = stringCal
--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const { strCal } = require('../app.js');
 
 const { expect } = chai;
 
@@ -12,12 +13,31 @@ function itAlwaysBe2() {
   expect(2).to.be.equal(2);
 }
 
+function empty(){
+  expect(strCal('')).to.be.equal(0)
+}
+
+function oneNum(){
+  expect(strCal('1')).to.be.equal(1)
+}
+
+function twoNum(){
+  expect(strCal('1,2')).to.be.equal(3)
+}
+
 // Start Example Behaviors
 function exampleBehaviors() {
   it('should be true', itAlwaysTrue);
   it('should be equal 2', itAlwaysBe2);
+
+  it('should be 0', empty);
+  it('should be 1', oneNum);
+  it('should be 3', twoNum);
 }
+
 
 // Start Describe
 describe('Example', exampleBehaviors);
+
+
 
